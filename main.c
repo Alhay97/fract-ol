7eaad41219b3377e15989fc66860f2f0bb70b6a8
Alhai Alhameli <aalhamel@lab2r4s2.42abudhabi.ae>
2022-07-07 16:29:04 +0400

    hi

--- main.c
+++ main.c
@@ -9,21 +9,35 @@ typedef struct	s_data
 	int		endian;
 }	t_data;
 
+
+void	alhai_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char *dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel/8));
+	*(unsigned int*)dst = color;
+}
+
 int main(void)
 {
-	void	*mlx;
+	void *alhai;
+	void *alhai_win;
 	t_data	img;
 
-	mlx = mlx_init();
-	img.img = mlx_new_image(mlx, 1920, 1080);
+	alhai = mlx_init();
+	alhai_win = mlx_new_window(alhai, 1920,1080, "Hello alhai"); //open a window
+	img.img = mlx_new_image(alhai, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
+			&img.endian);
+	alhai_mlx_pixel_put(&img, 100000, 1000000, 0x00FF01200);
+	mlx_put_image_to_window(alhai,alhai_win, img.img ,0,0);
+	mlx_loop(alhai);
 }
 // int main(void)
 // {
 // 	void *alhai;
 // 	void *alhai_win;
-// 	// void *alhai_image;
+// void *alhai_image;
 
 // 	alhai = mlx_init();
 // 	alhai_image = mlx_new_image(alhai,1920, 1080); //creates an image
