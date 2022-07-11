137d1e4fd042d7eaffd34095ecece08c0f5d8321
Alhai Alhameli <aalhamel@lab2r4s7.42abudhabi.ae>
2022-07-11 21:14:32 +0400

    goto events

--- main.c
+++ main.c
@@ -26,7 +26,6 @@ int main(void)
 	int i;
 	int j;
 	int k;
-	int b;
 	int a;
 	void *alhai_win;
 	t_data	img;
@@ -38,10 +37,21 @@ int main(void)
 	img.img = mlx_new_image(alhai, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 			&img.endian);
+
+
+	int b = 0;
+	int w = 0;
+	while(b < 1000)
+	{
+		w = 0;
+		while(w < 1000)
+			alhai_mlx_pixel_put(&img, b, w++, 0x00FFFF66);
+		b++;
+	} 
 	i = 1;
 	j = 1;
 	while(i < 1000 && j< 1000)
-		alhai_mlx_pixel_put(&img, i++, j++, 0x00FF0000);
+		alhai_mlx_pixel_put(&img, i++, j++, 0x0066FF99);
 	k = 1;
 	b = 1000;
 	while (k > 0 && b > 0)
@@ -55,20 +65,18 @@ int main(void)
 		alhai_mlx_pixel_put(&img, 1000, i--,0x00999900);
 	j = 1000;
 	while (j > 0)
-		alhai_mlx_pixel_put(&img, j--, 1,0x00FFFF00);
-	a = 1000;
-	while (a > 0)
-		alhai_mlx_pixel_put(&img,1,a--,0x00FFFFFF);
+		alhai_mlx_pixel_put(&img, j--, 1,0x0066FF99);
+	a = 1000;		
 
 	double x = 0;
 	double y = 0;
 	while (x < 100)
 	{
 		y = sqrt(pow(100, 2) -pow(x, 2));
-		alhai_mlx_pixel_put(&img, 500 + x, 500 + y, 0x00FF0000);
-		alhai_mlx_pixel_put(&img, 500 - x, 500 - y, 0x00FF0000);
-		alhai_mlx_pixel_put(&img, 500 - x, 500 + y, 0x00FF0000);
-		alhai_mlx_pixel_put(&img, 500 + x, 500 - y, 0x00FF0000);
+		alhai_mlx_pixel_put(&img, 500 + x, 500 + y, 0x00FFCC00);
+		alhai_mlx_pixel_put(&img, 500 - x, 500 - y, 0x0099FFFF);
+		alhai_mlx_pixel_put(&img, 500 - x, 500 + y, 0x006666FF);
+		alhai_mlx_pixel_put(&img, 500 + x, 500 - y, 0x00999900);
 		x++;
 	}
 
